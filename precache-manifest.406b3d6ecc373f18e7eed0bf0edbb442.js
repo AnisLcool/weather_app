b9f09842dbd80b0943e7a6dcccd31878fcac2236
AnisLcool <42942454+AnisLcool@users.noreply.github.com>
2020-04-08 21:56:16 +0100

    Updates

---
+++ precache-manifest.406b3d6ecc373f18e7eed0bf0edbb442.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d6a442fb3eded16f7cf7b83b36da09",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "1ed692aa660194a25a32",
+    "url": "/weather_app/static/css/main.a7b61a59.chunk.css"
+  },
+  {
+    "revision": "28c8952a976d931c9cc0",
+    "url": "/weather_app/static/js/2.5182a24e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather_app/static/js/2.5182a24e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed692aa660194a25a32",
+    "url": "/weather_app/static/js/main.0221b9f6.chunk.js"
+  },
+  {
+    "revision": "d35e960dabad868d53bf",
+    "url": "/weather_app/static/js/runtime-main.5c926925.js"
+  },
+  {
+    "revision": "4f31ae0124771189b244ece4724a5673",
+    "url": "/weather_app/static/media/Clear.4f31ae01.jpg"
+  },
+  {
+    "revision": "9e9338dd33813ced99171728391d7426",
+    "url": "/weather_app/static/media/Clouds.9e9338dd.jpg"
+  },
+  {
+    "revision": "29ae005878379b783301ea5a3cb316a0",
+    "url": "/weather_app/static/media/Fog.29ae0058.jpg"
+  },
+  {
+    "revision": "492c32715607ee62d8837a4ab515341f",
+    "url": "/weather_app/static/media/Snow.492c3271.jpg"
+  },
+  {
+    "revision": "c84d082e4feddae37f2bc0f65f77d4bc",
+    "url": "/weather_app/static/media/Thunderstorm.c84d082e.jpg"
+  },
+  {
+    "revision": "590a1eeba599beba16d461f0af1bb602",
+    "url": "/weather_app/static/media/cold.590a1eeb.jpg"
+  },
+  {
+    "revision": "01525709dfc600a32bae4dfd9793b14e",
+    "url": "/weather_app/static/media/rainy2.01525709.jpg"
+  },
+  {
+    "revision": "ef1ab7472889246b0ad47bbc31203b9d",
+    "url": "/weather_app/static/media/sunny1.ef1ab747.jpg"
+  },
+  {
+    "revision": "a4cd18545ab23c09e73fef22d8c93a18",
+    "url": "/weather_app/static/media/sunny2.a4cd1854.jpeg"
+  },
+  {
+    "revision": "dcfdfb4ea1873ee1043fdf1667edd52e",
+    "url": "/weather_app/static/media/warm.dcfdfb4e.jpg"
+  }
+]);
